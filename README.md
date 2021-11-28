65d50e41ae43aa1133a4c8eb023544cb29077780
Shikhar Panwar <shikharpanwar8888@gmail.com>
2021-11-28 23:42:55 +0530

    fixed logo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Rajufood-Webpage
-This is a simple one-page website featuring the Rajufood restaurant.
+# Omnifood-Webpage
+This is a simple one-page website featuring the Omnifood restaurant.
 Most html, css are written from scratch to practice front end development
--- resources/js/script.js
+++ resources/js/script.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     });
     
-    $('.js--scroll-to-Rajut').click(function () {
+    $('.js--scroll-to-start').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
     
